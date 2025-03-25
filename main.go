4710511f9cd2aa92925e7b26753d9e0e449ffc03
RaulCD3597 <raulcd3597@gmail.com>
2025-03-25 10:18:25 -0400

    read lines in a separate go routine

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
@@ -15,22 +16,44 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
 
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
+	c := getLinesChannel(f)
+	for line := range c {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	c := make(chan string)
+	currentLineContents := ""
+	go func() {
+		defer f.Close()
+		defer close(c)
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					fmt.Printf("read: %s\n", currentLineContents)
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := range len(parts) - 1 {
+				c <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
-		str := string(b[:n])
-		fmt.Printf("read: %s\n", str)
-	}
+	}()
+	return c
 }
